df5a0e4f527a18485336b810ea3b349fc1fce086
bhanuPrakashMa <147130893+bhanuPrakashMa@users.noreply.github.com>
2025-08-05 09:19:06 +0200

    Add files via upload

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,52 +1,58 @@
+from Layers import Base
 import numpy as np
-from Layers.Base import BaseLayer
 import copy
 
 
-class NeuralNetwork():
-    def __init__(self, optimizer):
+class NeuralNetwork(Base.BaseLayer):
+
+    def __init__(self, optimizer, weights_initializer, bias_initializer):
         super().__init__()
+
         self.optimizer = optimizer
+        self.weights_initializer = weights_initializer
+        self.bias_initializer = bias_initializer
         self.loss = []
         self.layers = []
         self.data_layer = None
         self.loss_layer = None
-        self.current_label_tensor = None
+        self.label_buffer = None
 
-    
+        return
+
     def forward(self):
-        input_data, self.current_label_tensor = self.data_layer.next()
-        for lay in range(len(self.layers)):
-            input_data = self.layers[lay].forward(input_data)
-        output_tensor = self.loss_layer.forward(input_data, self.current_label_tensor)
-        return output_tensor
 
+        input_tensor, self.label_buffer = self.data_layer.next()
+
+        for layer in self.layers:
+            input_tensor = layer.forward(input_tensor)
+
+        output = self.loss_layer.forward(input_tensor, self.label_buffer)
+
+        return output
 
     def backward(self):
-        error_data = self.loss_layer.backward(self.current_label_tensor)
-        for lay in reversed(self.layers):
-            error_data = lay.backward(error_data)
-
+        error_tensor = self.loss_layer.backward(self.label_buffer)
+        for layer in np.flip(self.layers):
+            error_tensor = layer.backward(error_tensor)
+        return
 
     def append_layer(self, layer):
         if layer.trainable:
-            layer.optimizer = copy.deepcopy(self.optimizer)
+            layer.initialize(self.weights_initializer, self.bias_initializer)
+            deep_copy = copy.deepcopy(self.optimizer)
+            layer.optimizer = deep_copy
         self.layers.append(layer)
-
+        return
 
     def train(self, iterations):
-        for _ in range(iterations):
-            error_val = self.forward()
-            self.loss.append(error_val)
+        for i in range(iterations):
+            loss = self.forward()
+            self.loss.append(loss)
             self.backward()
+        return
 
-    def test(self,input_tensor):
-        output = input_tensor
-        for lay in self.layers:
-            output = lay.forward(output)
-        return output
-
-
-
-
-
+    def test(self, input_tensor):
+        for layer in self.layers:
+            input_tensor = layer.forward(input_tensor)
+        results = input_tensor
+        return results
